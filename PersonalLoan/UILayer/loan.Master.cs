0646cf380995e29a91f2917593269d1ef46ad3fa
Adarsh Srivastava <770236@cognizant.com>
2019-05-17 09:24:29 +0530

    master

---
+++ PersonalLoan/UILayer/loan.Master.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace PersonalLoan.UILayer
+{
+    public partial class loan : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
